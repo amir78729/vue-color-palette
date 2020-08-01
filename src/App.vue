0c87edbb3d6abaf46551085b4287f825b6f1e43f
amir78729 <amir78729@gmail.com>
2020-08-01 15:39:36 +0430

    changing color from js hardcoded

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    hi
+  <div class=" container bg-light">
+    <button @click="changeColor" class="btn btn-primary">change color</button>
+    <div id="h" style="height: 100px; width: 100px; background-color: black"></div>
   </div>
 </template>
 
@@ -8,7 +9,12 @@
 export default {
   data () {
     return {
-      colors: []
+      colors: ['#FFEE00']
+    }
+  },
+  methods: {
+    changeColor () {
+      document.getElementById('h').style.backgroundColor = this.colors[0]
     }
   }
 }
