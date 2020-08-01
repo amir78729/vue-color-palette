6d27f451ed86e8c3142ef1c625c852094eac61fa
amir78729 <amir78729@gmail.com>
2020-08-01 18:45:20 +0430

    hover fade added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class=" container bg-light">
+  <div class=" container-fluid bg-light">
+    <div class="justify-content-center row header">
+      <h1 class="display-4 text-light">Color Palette</h1>
+    </div>
     <div class="row">
       <form>
         <div class="input-group">
@@ -16,16 +19,20 @@
 <!--    <div class="color" style="height: 100px; width: 100px; background-color: black"></div>-->
     <ul class="list-group bg-dark palette">
       <transition-group name="slide" type="in-out">
-        <li class="list-group-item color d-flex"
+        <li class="list-group-item color "
             v-for="(color, index) in colors"
             :key="color"
-            :style="{backgroundColor: colors[index]}">
-          <div class="flex-grow-1">
-            <label class="display-5">{{ color }}</label>
-          </div>
+            :style="{backgroundColor: colors[index]}"
+            >
+          <div  class="d-flex">
+            <div class="flex-grow-1">
+              <label class="list-title">{{ color }}</label>
+            </div>
 
-          <button class="btn list-btn btn-sm" @click="removeColor(index)">remove color</button>
-          <button class="btn list-btn btn-sm" @click="removeColor(index)">change color</button>
+            <button class="btn list-btn btn-sm" @click="removeColor(index)">remove color</button>
+            <button class="btn list-btn btn-sm" @click="changeColor(index)">change color</button>
+            <button class="btn list-btn btn-sm" @click="copyColor(index)">copy hex</button>
+          </div>
 
         </li>
       </transition-group>
@@ -39,7 +46,7 @@ export default {
   data () {
     return {
       colors: ['#FFEE00','#123123','#456456','#756765','#3215ac','#123346','#765345'],
-
+      hover: false,
       inputColor: 'select:',
     }
   },
@@ -61,8 +68,23 @@ export default {
 </script>
 
 <style>
+  .header{
+    background-image: url("https://wallup.net/wp-content/uploads/2018/09/25/629276-abstract-colorful.jpg");
+
+  }
   .color {
     margin-bottom: 5px;
+
+  }
+  .color button{
+    /*height: 0;*/
+    opacity: 0;
+    transition: all .2s;
+  }
+  .color:hover button{
+    /*height: auto;*/
+    opacity: 1;
+    transition: all .2s;
   }
   .color button{
     margin: 0 5px;
@@ -86,7 +108,7 @@ export default {
   }
   .slide-leave-active{
     animation: slide-out 1s ease-out forwards;
-    transition: opacity 0.5s;
+    transition: opacity 0s;
     opacity: 0;
     position: absolute;
   }
@@ -113,5 +135,11 @@ export default {
     padding: 5px;
     border-radius: 10px;
     transition: transform 1s;
+  }
+  .list-title{
+    background-color: #00000077;
+    border-radius: 5px;
+    padding: 4px 12px;
+    color: white;
   }
 </style>
